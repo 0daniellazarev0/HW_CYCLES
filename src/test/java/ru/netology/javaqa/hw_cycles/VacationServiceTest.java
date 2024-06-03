aef925cdce7fc9bb6cedbda91939e47165538346
Daniel Lazarev <daniil.lt@hotmail.com>
2024-06-03 22:18:56 +0300

    Task 2 done

--- src/test/java/ru/netology/javaqa/hw_cycles/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa/hw_cycles/VacationServiceTest.java
@@ -2,14 +2,12 @@ package ru.netology.javaqa.hw_cycles;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "100000, 150000, 60000, 2",
-            "10000, 20000, 3000, 3"
-    })
+    @CsvFileSource (files="src/test/resources/vacationTestData.csv")
     public void checkWhenYouAreOnVacation(int income, int threshold, int expenses, int expectedResult) {
         VacationService service = new VacationService();
         int vacationMonths = service.calculate(income, threshold, expenses);
